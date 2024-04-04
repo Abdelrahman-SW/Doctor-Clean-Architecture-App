490bc8174ab90c29ec019cbf536d1b6e49f3fdb6
AbdelrhamanKhaled <Bedona213@gmail.com>
2024-04-04 17:11:00 +0200

    link the app with firebase project and integrate (crashlytics - firestore - storage) for firebase

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.firebaseCrashlytics) apply false
+}
+
+// apply false :  sometimes you might want to apply a plugin only under certain conditions,
+// like when a specific task is executed or when a certain property is set.
